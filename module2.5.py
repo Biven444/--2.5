5869b9307aeb4b4025cfe1c2f3718cdf95db87db
Biven444 <biven2010@gmail.com>
2024-10-31 09:22:04 +0300

    Update module2.5.py

--- module2.5.py
+++ module2.5.py
@@ -5,6 +5,8 @@ def get_matrix(n, m, value):
         for j in range(m):
             matrix[i].append(value)
     print(matrix)
-get_matrix(4,2,13)
+result_1 = get_matrix(2,2,10)
+result_2 = get_matrix(3,5,42)
+result_3 = get_matrix(4,2,13)
 
 
